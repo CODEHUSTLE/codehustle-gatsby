61cbeaf040fe42982547504c0e5968d450c08f42
Jerry Hirsch <codehustle@GovDev.local>
2020-06-15 22:53:47 -0400

    Initial Push

---
+++ src/components/layout.js
@@ -0,0 +1,52 @@
+import React from "react"
+import { css } from "@emotion/core"
+import { Link, useStaticQuery, graphql } from "gatsby"
+import SEO from "../components/seo"
+import { rhythm } from "../utils/typography"
+
+export default function Layout({ children }) {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+  )
+
+  return (
+    <div
+      css={css`
+        margin: 0 auto;
+        max-width: 700px;
+        padding: ${rhythm(2)};
+        padding-top: ${rhythm(1.5)};
+      `}
+    >
+      <SEO title={`🕺`} />
+      <Link to={`/`}>
+        <h3
+          css={css`
+            margin-bottom: ${rhythm(2)};
+            display: inline-block;
+            font-style: normal;
+          `}
+        >
+          {data.site.siteMetadata.title}
+        </h3>
+      </Link>
+      <Link
+        to={`/about/`}
+        css={css`
+          float: right;
+        `}
+      >
+        About
+      </Link>
+      {children}
+    </div>
+  )
+}
